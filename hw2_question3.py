d90e5538a745ae5123db3b5f1e53f4c05f4a2c2b
Jaeun Delio <jaeundelio@Jaeuns-MacBook-Pro.local>
2023-11-09 21:34:21 -0800

    Homework 2

--- hw2_question3.py
+++ hw2_question3.py
@@ -1,21 +1,43 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # init driver
-driver = webdriver.Chrome(executable_path='/Users/jaeundelio/Desktop/python-selenium-automation/chromedriver')
+service = Service(executable_path='/Users/jaeundelio/Desktop/python-selenium-automation/chromedriver')
+driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
 driver.get('https://www.amazon.com/')
 
-# click Orders button
-search = driver.find_element(By.ID, 'nav-orders').click()
-
 # find Sing-in
-driver.find_element(By.CSS_SELECTOR, "[class='a-spacing-small']")
+driver.find_element(By.ID,'nav-link-accountList-nav-line-1').click()
 
-# verify
-assert 'ap/signin?' in driver.current_url, f'{driver.current_url} Is not the right page'
+# find amazon logo
+driver.find_element(By.XPATH, "//div[@class='a-section a-padding-medium auth-workflow']//i[@class='a-icon a-icon-logo']")
 
-quit()
+# find Email field
+driver.find_element(By.ID, 'ap_email')
+
+# find Continue button
+driver.find_element(By.ID, 'continue')
+
+# find Conditions of use link
+driver.find_element(By.XPATH, "//a[text()='Conditions of Use']")
+
+# find Privacy Notice link
+driver.find_element(By.XPATH, "//a[text()='Privacy Notice']")
+
+# find Need help link
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+# find Forgot your password link - N/A
+# find Other issues with Sign-In link - N/A
+
+# find Create your Amazon account button
+driver.find_element(By.ID, 'createAccountSubmit')
+
+print('Test Passed')
+
+driver.quit()
