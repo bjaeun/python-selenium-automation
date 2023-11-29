d7b9b835812ccfb3e3af0260706d606e7e3f8b1e
bjaeun <104389373+bjaeun@users.noreply.github.com>
2023-11-28 22:49:08 -0800

    hw6_question2.py

--- hw6_question2.py
+++ hw6_question2.py
@@ -1,18 +1,15 @@
 from selenium.webdriver.common.by import By
-from time import sleep
-from pages.base_page import Page
+
+@given('open target main page')
+def open_target_main_page(context):
+    context.app.base_page.open_url(context, 'https://www.target.com/')
 
 
-class SearchResultsPage(Page):
-    LOCATOR = (By.CSS_SELECTOR, "[href*='Cart.svg']")
-    CART = (By.XPATH, "//h1[text()='Your cart is empty']")
+@when('click cart button')
+def click_orders_button(context):
+    context.app.main_page.click_cart()
 
-    def open_target_main_page(self):
-        self.open_url('https://www.target.com/')
 
-    def click_orders_button(self):
-        self.find_element(*self.LOCATOR).click()
-
-    def verify_message_is_shown(self):
-        actual_result = self.find_element(*self.CART).text
-        assert 'Your cart is empty' in actual_result, f'{actual_result} Is not the right page'
+@then('Verify message is shown')
+def verify_message_is_shown(context):
+    context.app.cart_page.verify_cart_msg()
